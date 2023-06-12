612dc16e12f08c2e6ee1c26cd1761a88736415f3
ChampionVikash <4355vikashkumar4355@gmail.com>
2023-06-12 13:39:57 +0530

    Added basic crud repo and extended it in like repo

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,7 +1,10 @@
 import Tweet from '../models/tweet.js'
+import CrudRepository from './crud-repository.js';
 
-class TweetRepository {
-    
+class TweetRepository extends CrudRepository{
+    constructor() {
+        super(Tweet);
+    }
     async create(data) {
         try {
             const tweet = await Tweet.create(data);
@@ -11,27 +14,9 @@ class TweetRepository {
         }
     }
 
-    async get(id) {
-        try {
-            const tweet = await Tweet.findById(id);
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
     async getWithComments(id) {
         try {
             const tweet = await Tweet.findById(id).populate({path: 'comments'}).lean();
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async destroy(id) {
-        try {
-            const tweet = await Tweet.findByIdAndRemove(id);
             return tweet;
         } catch (error) {
             console.log(error);
